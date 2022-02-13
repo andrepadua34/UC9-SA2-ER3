ce0ad143d2d978cb269e8d4f32bccb0ad58a1bb0
andrepadua34 <andrepadua34@gmail.com>
2022-02-13 17:45:13 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function mostrarPopup(){
+    window.alert("Sejam Bem Vindos ao Melhor Site de Games do Universo!")
+}
+
+let email = document.getElementById("campo-email");
+
+function enviaEmail(){
+    let emailDigitado = email.value;
+    console.log(emailDigitado)
+}
